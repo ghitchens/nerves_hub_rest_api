bdd855afc788a4d6d42435a7d6ff10740cbe6349
Chris Dutton <chris@rosepointnav.com>
2015-04-11 23:07:07 -0700

    Rewrote jrtp_bridge in elixir, added elixir tests. Leaving erlang module and tests

--- lib/jrtp_bridge.ex
+++ lib/jrtp_bridge.ex
@@ -1,2 +1,212 @@
 defmodule JrtpBridge do
+
+  alias :cowboy_req, as: CowboyReq
+
+  def init(_transport, req, state) do
+    {:upgrade, :protocol, :cowboy_rest, req, state}
+  end
+
+  def rest_init(req, handler_opts) do
+    {:ok, req, handler_opts}
+  end
+
+  def resource_exists(req, state) do
+    case Hub.fetch(request_path(req)) do
+      {_, :error} -> {false, req, state}
+      {_, _} -> {true, req, state}
+    end
+  end
+
+  def allowed_methods(req, state) do
+    {["GET", "PUT"], req, state}
+  end
+
+  def content_types_provided(req, state) do
+    {[{"application/merge-patch+json", :rfc7386_provider},
+      {"application/json", :json_provider},
+      {"text/html", :html_provider},
+      {"text/plain", :text_provider}
+      ], req, state}
+  end
+
+  def rfc7386_provider(req, state) do
+    path = request_path(req)
+    {vers_header, req} = CowboyReq.header("x-since-version", req)
+    vreq = vheader_to_ver(vers_header)
+    {long_poll, req} = CowboyReq.header("x-long-poll", req)
+    {long_poll_header_value, req} = CowboyReq.header("x-long-poll-timeout", req)
+    long_poll_timeout = case long_poll_header_value do
+      :undefined -> 30000
+      n -> String.to_integer n
+    end
+    {vres, tree} = case long_poll do
+      :undefined -> Hub.deltas(vreq, path)
+      _ ->
+        start_result = case Dict.get(state, :on_wait_start) do
+          nil -> nil
+          wait_start_fn -> wait_start_fn.()
+        end
+        Hub.watch(path, [])
+        r = wait_for_version_after(vreq, path, long_poll_timeout)
+        Hub.unwatch(path)
+        case Dict.get(state, :on_wait_end) do
+          nil -> nil
+          wait_end_fn -> wait_end_fn.(start_result)
+        end
+        r
+    end
+
+    {vlock_req, _} = vreq
+    {vlock_res, _} = vres
+
+    #{set_time, req} = CowboyReq.header("x-set-time", req)
+    req = CowboyReq.set_resp_header("x-version", ver_to_vheader(vres), req)
+    req = invoke_response_hook_if_present(req, state)
+
+    req = case vlock_res do
+      v when v == vlock_req -> req
+      _ -> CowboyReq.set_resp_header("content-type", "application/json", req)
+    end
+
+    conditional_get = (vreq > 0)
+    case {vreq, tree} do
+      {{:undefined, 0}, []} -> {"", req, state}
+      {_, []} ->
+        {:ok, req} = CowboyReq.reply(304, [], req)
+        {:halt, req, state}
+      _ ->
+        body = erl_to_json(tree)
+        {"#{body}\n", req, state}
+    end
+  end
+
+  def json_provider(req, state) do
+    path = request_path(req)
+    {vres, tree} = Hub.deltas({:undefined, 0}, path)
+    req = CowboyReq.set_resp_header("x-version", ver_to_vheader(vres), req)
+    req = invoke_response_hook_if_present(req, state)
+    case tree do
+      [] -> {"", req, state}
+      _ ->
+        body = erl_to_json tree
+        {body <> "\n", req, state}
+    end
+  end
+
+  def html_provider(req, state) do
+    header = "<html><head><meta charset=\"utf-8\">#{Dict.get(state, :webpage_title)}</head><body><pre>"
+    footer = "</pre></body></html>"
+    {body, reply, state} = json_provider(req, state)
+    {header <> body <> footer, reply, state}
+  end
+
+  def text_provider(req, state) do
+    json_provider(req, state)
+  end
+
+  def vheader_to_ver(version_header_value) do
+    case version_header_value do
+      :undefined -> {:undefined, 0}
+      s ->
+        case String.split(s, ":") do
+          [vlock, vs] -> {vlock, String.to_integer(vs)}
+          _ -> {:undefined, 0}
+        end
+    end
+  end
+
+  def ver_to_vheader({vlock, ver}) do
+    bver = :erlang.list_to_binary(:erlang.integer_to_list(ver))
+    "#{vlock}:#{bver}"
+  end
+
+  def content_types_accepted(req, state) do
+    {[
+      {{"application", "merge-patch+json", []}, :rfc7386_acceptor},
+      {{"application", "json", []}, :json_acceptor},
+    ], req, state}
+  end
+
+  def rfc7386_acceptor(req, state) do
+    json_acceptor(req, state)
+  end
+
+  def json_acceptor(req, state) do
+    {:ok, request_body, req} = CowboyReq.body(req)
+    proposed_changes = json_to_erl(request_body)
+    case Hub.request(request_path(req), proposed_changes) do
+      {:changes, vres, changes} ->
+        change_json = erl_to_json(changes)
+        response_body = change_json <> "\n"
+        bver = ver_to_vheader(vres)
+        req = CowboyReq.set_resp_header("x-version", bver, req)
+        req = CowboyReq.set_resp_body(response_body, req)
+        {true, req, state}
+      {:nochanges, vres, changes} ->
+        bver = ver_to_vheader(vres)
+        req = CowboyReq.set_resp_header("x-version", bver, req)
+        {:ok, req} = CowboyReq.reply(304, [], req)
+        {:halt, req, state}
+      :ok ->
+        {:ok, req} = CowboyReq.reply(202, [], req)
+        {:halt, req, state}
+      _ ->
+        {:ok, req} = CowboyReq.reply(400, [], req)
+        {:halt, req, state}
+    end
+  end
+
+  def invoke_response_hook_if_present(req, state) do
+    case Dict.get(state, :json_provider_hook) do
+      nil -> req
+      resp_hook_fn -> resp_hook_fn.(req)
+    end
+  end
+
+  def request_path(req) do
+    {tokens, _} = CowboyReq.path_info(req)
+    tokens
+  end
+
+  def wait_for_version_after(vreq, path, long_poll_timeout) do
+    case Hub.deltas(vreq, path) do
+      {vreq, _} ->
+        receive do
+          _ -> wait_for_version_after(vreq, path, long_poll_timeout)
+        after
+          long_poll_timeout ->
+            Hub.deltas(vreq, path)
+        end
+      {vres, []} ->
+        wait_for_version_after(vres, path, long_poll_timeout)
+      {vres, change_tree} ->
+        {vres, change_tree}
+    end
+  end
+
+  def erl_to_json(term) do
+    :jsx.encode(term, [{:space, 1}, {:indent, 2}, {:pre_encode, &deatomize/1}])
+  end
+
+  def json_to_erl(json) do
+    case :jsx.decode(json, [:relax, {:labels, :atom}, {:post_decode, &atomize/1}]) do
+      {:incomplete, _} -> throw(:error)
+      erl -> erl
+    end
+  end
+
+  def atomize(<< h :: size(1)-binary, b :: binary>>) do
+    case h do
+      "#" -> :erlang.binary_to_atom(b, :utf8)
+      _ -> h <> b
+    end
+  end
+
+  def atomize(o), do: o
+
+  def deatomize(true), do: true
+  def deatomize(false), do: false
+  def deatomize(nil), do: nil
+  def deatomize(a) when is_atom(a), do: :erlang.atom_to_binary(a, :utf8)
+  def deatomize(o), do: o
 end
---
+++ test/jrtp_bridge_ex_test.exs
@@ -0,0 +1,207 @@
+defmodule JrtpBridgeTest do
+
+  use ExUnit.Case
+
+  alias :cowboy, as: Cowboy
+  alias :cowboy_req, as: CowboyReq
+  alias :cowboy_router, as: CowboyRouter
+
+  HTTPotion.start
+
+  # configure and start a basic http server on port 8088 using cowboy
+  # that puts the jrtp bridge at the /jrtp/ directory for test purposes.
+
+  @test_http_port 8088
+  @test_http_root "localhost:#{@test_http_port}/jrtp/"
+  @silly_path [:tests, :silly]
+  @silly_http @test_http_root <> "tests/silly/"
+  @webpage_title "<title>TESTING JRTP</title>"
+
+  Hub.start
+
+  def connect_pinger do
+    :timer.sleep 1000
+    connect_pinger
+  end
+
+  # a funcntion to test response hooks
+  def json_provider_hook_test_fn(req) do
+    case CowboyReq.header("x-resp-hook-input", req) do
+      {:undefined, r} -> r
+      {t, r} -> CowboyReq.set_resp_header "x-resp-hook-result", t, r
+    end
+  end
+
+  dispatch = CowboyRouter.compile [ {:_, [
+      {"/jrtp/[...]", JrtpBridge, %{
+        on_wait_start: (fn -> spawn(&JrtpBridgeTest.connect_pinger/0) end),
+        on_wait_end:   &(:erlang.exit(&1, :disconnected)),
+        json_provider_hook: &(JrtpBridgeTest.json_provider_hook_test_fn/1),
+        webpage_title: @webpage_title
+      }} ]} ]
+
+  {:ok, _pid} = Cowboy.start_http :http, 10, [port: @test_http_port],
+                                              [env: [dispatch: dispatch] ]
+
+  test "webserver is up and returns root of url space as json when asked for json" do
+    # need to put a key there otherwise can get a 304
+    Hub.update [ "testing" ], [ "a_key": "a_value" ], []
+    resp = HTTPotion.get @test_http_root, headers: ["Accept": "application/json"]
+    headers = resp.headers
+    assert resp.status_code == 200
+    assert {:ok, "Cowboy"} = Keyword.fetch(headers, :server)
+    assert {:ok, "application/json"} = Keyword.fetch(headers, :'content-type')
+  end
+
+  test "webserver returns correct title under html" do
+    resp = HTTPotion.get @test_http_root, headers: ["Accept": "text/html"]
+    body = resp.body
+    assert String.contains? body, @webpage_title
+  end
+
+  test "response_hook modifies headers when it's present in json request" do
+    resp = HTTPotion.get @test_http_root, headers: [
+      "Accept": "application/json",
+      "x-resp-hook-input": "TESTVALUE"
+    ]
+    headers = resp.headers
+    assert resp.status_code == 200
+    assert {:ok, "TESTVALUE"} = Keyword.fetch(headers, :'x-resp-hook-result')
+    assert {:ok, "application/json"} = Keyword.fetch(headers, :'content-type')
+  end
+
+  test "response_hook modifies merge-patch when it's present" do
+    resp = HTTPotion.get @test_http_root, headers: [
+      "Accept": "application/merge-patch+json",
+      "x-resp-hook-input": "TEST2VALUE"
+    ]
+    headers = resp.headers
+    assert resp.status_code == 200
+    assert {:ok, "TEST2VALUE"} = Keyword.fetch(headers, :'x-resp-hook-result')
+    assert {:ok, "application/json"} = Keyword.fetch(headers, :'content-type')
+  end
+
+  test "webserver is up and returns root of url space as merge-patch+json if allowed" do
+    # need to put a key there otherwise can get a 304
+    Hub.update [ "testing" ], [ "a_key": "another_value" ], []
+    resp = HTTPotion.get @test_http_root
+    headers = resp.headers
+    assert resp.status_code == 200
+    assert {:ok, "Cowboy"} = Keyword.fetch(headers, :server)
+    assert {:ok, "application/json"} = Keyword.fetch(headers, :'content-type')
+  end
+
+  test "webserver returns merge-patch+json when changes occur changes" do
+    # need to put a key there otherwise can get a 304
+    {{vlock, vnum}, _} = Hub.fetch []
+    Hub.update [ "testing" ], [ "a_key": "yet_another_value" ], []
+    resp = HTTPotion.get @test_http_root, headers: ["x-since-version": "#{vlock}:#{vnum}"]
+    headers = resp.headers
+    assert resp.status_code == 200
+    assert {:ok, "Cowboy"} = Keyword.fetch(headers, :server)
+    assert {:ok, "application/merge-patch+json"} = Keyword.fetch(headers, :'content-type')
+  end
+
+  # helpers for the JSON tests
+
+  @doc "Return the value of a header on an integer - key can be atom or str"
+  def header(resp, key) do
+    unless is_atom(key) do
+      key = String.to_atom(key)
+    end
+    assert {:ok, result} = Keyword.fetch(resp.headers, key)
+    {:ok, result}
+  end
+
+  @doc "Return the integer value of the version header value"
+  def iver(vhdr) do
+    [_, ver] = String.split(vhdr, ":")
+    :erlang.binary_to_integer(ver)
+  end
+
+  @doc "Return term from decoded json body of the response"
+  def jterm(resp) do
+    #{:ok, content_type} = header resp, "content-type"
+    :jrtp_bridge.json_to_erl(resp.body)
+  end
+
+  test "JSON interface to querying hub including versions and deltas" do
+
+    test_data1 = [ basic_key: "yes", another_key: "no", with: 3 ]
+    test_data2 = [ basic_key: "yes", another_key: "maybe", with: 3 ]
+    test_rootkey = :jrtp_temp_test_key
+    test_subkey = :webtest
+    test_path = [ test_rootkey, test_subkey ]
+    test_uri = "#{@test_http_root}#{test_rootkey}/#{test_subkey}"
+    test_root_uri = "#{@test_http_root}#{test_rootkey}"
+
+    # attempt to get data from our test area.  This should generate
+    # a 404 since we haven't created the data there yet
+
+    resp = HTTPotion.get test_uri
+    assert 404 == resp.status_code  # shouldn't be there yet
+
+    # now write some test data to the hub, and then query the HTTP
+    # interface to see if we actually get the data back
+
+    Hub.update test_path, test_data1
+    resp = HTTPotion.get test_uri
+    assert 200 == resp.status_code  # should be there now
+    assert {:ok, "application/json"} = header resp, "content-type"
+    assert {:ok, first_vers} = header resp, "x-version"
+    assert Keyword.equal?(jterm(resp), test_data1)
+
+    # test updating the hub and seeing if we can query the HTTP
+    # interface to see the updated version and updated data
+
+    Hub.update test_path, test_data2
+    resp = HTTPotion.get test_uri
+    assert 200 == resp.status_code
+    assert Keyword.equal?(jterm(resp), test_data2)
+    assert {:ok, second_vers} = header resp, "x-version"
+    assert second_vers != first_vers
+    [a, _b] = String.split(second_vers, ":")
+    assert String.length(a) > 5
+    assert iver(second_vers) == (iver(first_vers) + 1)
+
+    # now make sure that if we ask for changes since a particular
+    # version, that we only get back changes since that version
+
+    resp = HTTPotion.get test_uri, headers: [ "x-since-version": first_vers]
+    assert 200 == resp.status_code
+    assert {:ok, second_vers} == header resp, "x-version"
+    assert {:ok, "application/merge-patch+json"} = header resp, "content-type"
+    assert jterm(resp) == [ another_key: "maybe" ]
+
+    # make sure we timeout properly by reqesting a uri with long polling,
+    # and sending a change with no changes, and hopefully we don't get response
+
+    spawn fn ->
+      :timer.sleep 300
+      Hub.update test_path, test_data2
+      :timer.sleep 50
+      Hub.update test_path, test_data2
+      :timer.sleep 50
+      Hub.update test_path, test_data2
+      :timer.sleep 50
+      Hub.update test_path, test_data2
+    end
+
+    resp = HTTPotion.get test_root_uri, headers: [
+			"x-since-version": second_vers,
+			"x-long-poll": true,
+			"x-long-poll-timeout": "1000"
+		]
+
+    assert {:ok, second_vers} == header resp, "x-version"
+	assert 304 == resp.status_code			 # should be NOT MODIFIED
+
+	# now make the same change, and ask if changes happened
+
+    Hub.update test_path, test_data2
+    resp = HTTPotion.get test_uri, headers: [ "x-since-version": second_vers]
+    assert 304 == resp.status_code			 # should be NOT MODIFIED
+    assert {:ok, second_vers} == header resp, "x-version"
+
+  end
+end
